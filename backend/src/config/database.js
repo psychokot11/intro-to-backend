b0fcd397d936cf0ccd037ca2a6a3799394a5fff8
Zofia Stawarska <z.stawarska@gmail.com>
2025-12-13 00:47:00 +0100

    initial config

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect
+        (`${process.env.MONGODB_URI}`)
+        console.log("Database connected successfully:", connectionInstance.connection.host);
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+        process.exit(1);
+    }
+};
