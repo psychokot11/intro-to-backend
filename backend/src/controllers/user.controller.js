97237b1db4fbe65d12779e8462d0eea921c01dd3
Zofia Stawarska <z.stawarska@gmail.com>
2025-12-16 23:10:55 +0100

    hash the password

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,18 +4,15 @@ const registerUser = async (req, res) => {
     try {
         const { username, password, email } = req.body;
 
-        //basic validation
         if (!username || !password || !email) {
             return res.status(400).json({ message: "All fields are required." });
         }
 
-        // check if user already exists
         const existing = await User.findOne({ email: email.toLowerCase() });
         if (existing) {
             return res.status(409).json({ message: "User already exists." });
         }
 
-        //create new user
         const newUser = new User({
             username,
             password,
@@ -31,6 +28,24 @@ const registerUser = async (req, res) => {
         });
     } catch (error) {
         res.status(500).json({ message: "Server error.", error: error.message });
+    }
+};
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ 
+            email: email.toLowerCase() 
+        });
+
+        if (!user) return res.status(404).json({ 
+            message: "User not found." 
+        });
+
+        if (user.password !== password) return;
+    } catch (error) {
+
     }
 };
 
