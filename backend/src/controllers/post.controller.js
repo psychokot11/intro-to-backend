341787dc5542f37ce84b71a68d9295b9eaae6c48
Zofia Stawarska <z.stawarska@gmail.com>
2025-12-27 13:24:58 +0100

    post create

---
+++ backend/src/controllers/post.controller.js
@@ -0,0 +1,22 @@
+import { Post } from "../models/post.model.js";
+
+const createPost = async (req, res) => {
+    try {
+        const { name, description, age } = req.body;
+        
+        if (!name || !description || !age) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        
+        const post = await Post.create({ name, description, age });
+
+        res.status(201).json({
+            message: "Post created successfully",
+            post
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message });
+    }
+};
+
+export { createPost };
