f324e6d8b761b59d3bfdf2345343b6d57a1f4760
Zofia Stawarska <z.stawarska@gmail.com>
2026-01-02 18:44:45 +0100

    save as a starter

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,17 +2,5 @@ import express from "express";
 
 const app = express();
 
-app.use(express.json());
-
-//routes import
-import userRouter from "./routes/user.route.js";
-import postRouter from "./routes/post.route.js";
-
-//routes declaration
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/posts", postRouter);
-
-//example route: http://localhost:4000/api/v1/users/register
-
 
 export default app;
